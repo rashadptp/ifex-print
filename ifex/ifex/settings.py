17540d0e4a3ad3d85685db3ccfe907fff517a0c3
Muhammed Rashad PTP <113594567+rashadptp@users.noreply.github.com>
2025-08-25 11:24:17 +0400

    Update settings.py

--- ifex/ifex/settings.py
+++ ifex/ifex/settings.py
@@ -76,14 +76,13 @@ WSGI_APPLICATION = 'ifex.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ifexdb',
-        'USER': 'root',
-        'PASSWORD': 'junaid2025',
-        'HOST': 'localhost',  # Or '127.0.0.1' if needed
-        'PORT': '3306',       # Default MySQL port
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -129,3 +128,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
